1b55d2a8a96277eda968b242a3dda953597274ab
Raestkjoot <gunnarmagnum@gmail.com>
2025-09-02 23:16:31 +0200

    transform simplification

--- Source/Gameplay/Transform.h
+++ Source/Gameplay/Transform.h
@@ -4,31 +4,7 @@
 
 class Transform {
 public:
-    inline glm::vec3 GetTranslation() const { return _translation; }
-    inline void SetTranslation(const glm::vec3& translation) { _translation = translation; _dirty = true; }
-
-    inline glm::vec3 GetRotation() const { return _rotation; }
-    inline void SetRotation(const glm::vec3& rotation) { _rotation = rotation; _dirty = true; }
-
-    inline glm::vec3 GetScale() const { return _scale; }
-    inline void SetScale(const glm::vec3& scale) { _scale = scale; _dirty = true; }
-
-    glm::mat4 GetTranslationMatrix() const;
-    glm::mat4 GetRotationMatrix() const;
-    glm::mat4 GetScaleMatrix() const;
-
-    glm::mat4 GetTransformMatrix() const;
-
-    bool IsDirty() const;
-
     glm::vec3 position {0.0f, 0.0f, 0.0f};
-
-private:
-    glm::vec3 _translation {0.0f, 0.0f, 0.0f};
-    glm::vec3 _rotation {0.0f, 0.0f, 0.0f};
-    glm::vec3 _scale {1.0f, 1.0f, 1.0f};
-
-    // Cached matrix
-    mutable glm::mat4 _matrix {1.0f};
-    mutable bool _dirty = false;
+    glm::vec3 rotation {0.0f, 0.0f, 0.0f};
+    glm::vec3 scale {0.0f, 0.0f, 0.0f};
 };
